16da6cc5e6a4074e69804e0324ce07182c7a248e
refracta <58779799+refracta@users.noreply.github.com>
2022-09-10 06:20:12 +0900

    Implement side-menu

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,18 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {FaGem, FaHeart} from "react-icons/fa";
+
+import {ProSidebar, Menu, MenuItem, SubMenu, SidebarHeader, SidebarFooter, SidebarContent} from 'react-pro-sidebar';
+
+import 'react-pro-sidebar/dist/css/styles.css';
+import Layout from "./Layout";
+import './styles/App.scss';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Layout/>
+    );
 }
 
 export default App;
