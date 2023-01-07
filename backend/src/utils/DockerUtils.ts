f74758ff18044d7b98461521ede2056f74d0a4bc
refracta <58779799+refracta@users.noreply.github.com>
2023-01-07 23:07:17 +0900

    Add legacy upload function

--- backend/src/utils/DockerUtils.ts
+++ backend/src/utils/DockerUtils.ts
@@ -1,4 +1,6 @@
-import Docker, {Container, ContainerInfo, Exec} from "dockerode";
+import Docker, {Container, ContainerInfo} from "dockerode";
+import fs from "fs";
+import {Duplex} from "stream";
 
 export default class DockerUtils {
     static async getContainerByName(docker: Docker, name: string): Promise<{ container: Container, containerInfo: ContainerInfo }> {
@@ -15,9 +17,26 @@ export default class DockerUtils {
                 Cmd: ['/bin/bash', '-c', command],
                 AttachStdin: true,
                 AttachStdout: true
-            }, async function (err, exec) {
-                let stream = await (exec as Exec).start({hijack: true, stdin: true});
-                stream.on('finish', resolve);
+            }, async (err, exec) => {
+                let stream = await exec?.start({hijack: true, stdin: true});
+                stream?.on('finish', resolve);
+            });
+        });
+    }
+
+    static uploadFile(docker: Docker, container: Container, localPath: string, remotePath: string) {
+        return new Promise(resolve => {
+            container.exec({
+                Cmd: ['/bin/bash', '-c', 'base64 -d > ' + remotePath],
+                AttachStdin: true,
+                AttachStdout: true
+            }, async (err, exec) => {
+                exec?.start({hijack: true, stdin: true}, function (err, stream) {
+                    let readStream = fs.createReadStream(localPath, 'base64');
+                    readStream.pipe(stream as Duplex)
+                    docker.modem.demuxStream(stream as Duplex, process.stdout, process.stderr);
+                    stream?.on('finish', resolve);
+                });
             });
         });
     }
