673650a7cf133fc3c3da259c67ce362abb466ad7
refracta <58779799+refracta@users.noreply.github.com>
2023-03-02 14:41:53 +0900

    Add link to navbar

--- frontend/src/components/sidebar/menu/impl/NavbarMenu.tsx
+++ frontend/src/components/sidebar/menu/impl/NavbarMenu.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import {Container, Navbar} from "react-bootstrap";
+import {Link} from "react-router-dom";
 
 interface Props {
     setToggled: (value: boolean) => void
@@ -9,7 +10,7 @@ export default function NavbarMenu({setToggled}: Props) {
     return <Navbar id="main-navbar" expand="md" variant="dark">
         <Container>
             <Navbar.Brand href="#home">
-                Model executor
+                <Link to='/' style={{textDecoration: 'none', color: 'white'}}>Model executor</Link>
             </Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" onClick={() => setToggled(true)}/>
         </Container>
